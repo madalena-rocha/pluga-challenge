708bfed57299650e00ec1ff299b9b27f60c5068a
madalena-rocha <rochamada1997m@gmail.com>
2025-02-20 16:39:43 -0300

    :lipstick: set styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 function App() {
-  return <h1>Pluga</h1>;
+  return (
+    <>
+      <h1>Pluga</h1>
+      <a href="">fsfefs</a>
+      <button>fdfsdf</button>
+      <ul>
+        <li>dfsdfd</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1a1a1a",
+    BACKGROUND_800: "#242424",
+
+    WHITE: "rgba(255, 255, 255, 0.87)",
+    BLUE: "#00B6F8",
+
+    GRAY_200: "#888",
+  },
+};
