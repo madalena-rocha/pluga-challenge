80b9060962d908b96f46ba2ddb06d72fd8b27246
madalena-rocha <rochamada1997m@gmail.com>
2025-02-21 11:04:38 -0300

    :sparkles: add component Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
+import { FiSearch } from "react-icons/fi";
+
+import { Input } from "./components/Input";
+
 function App() {
   return (
     <>
+      <Input placeholder="Buscar ferramenta" icon={FiSearch} />
       <h1>Pluga</h1>
-      <a href="">fsfefs</a>
-      <button>fdfsdf</button>
-      <ul>
-        <li>dfsdfd</li>
-      </ul>
     </>
   );
 }
