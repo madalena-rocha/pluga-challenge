6dd588955b10e9c1388b6fb97624af507b04f488
madalena-rocha <rochamada1997m@gmail.com>
2025-02-22 10:23:28 -0300

    :sparkles: fetch data from JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { FiSearch } from "react-icons/fi";
 
@@ -8,16 +8,27 @@ import { Modal } from "./components/Modal";
 
 function App() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pluga.co/ferramentas_search.json")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error("Erro ao buscar dados:", error));
+  }, []);
 
   return (
     <>
       <Input placeholder="Buscar ferramenta" icon={FiSearch} />
 
-      <Card
-        setIsModalOpen={setIsModalOpen}
-        icon="https://assets.pluga.co/apps/icons/omie/omie-icon.svg"
-        name="Omie"
-      ></Card>
+      {data.map((item) => (
+        <Card
+          key={item.app_id}
+          icon={item.icon}
+          name={item.name}
+          setIsModalOpen={setIsModalOpen}
+        />
+      ))}
 
       {isModalOpen && <Modal setIsMenuOpen={setIsModalOpen} />}
     </>
